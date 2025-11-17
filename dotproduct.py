25f9d7364d2162533e534079fd2f26b94ebbf5ee
Your Name <you@example.com>
2025-11-17 15:31:00 +0530

    small improvement

--- dotproduct.py
+++ dotproduct.py
@@ -1,4 +1,5 @@
-int x= "Enter the first number"
-int y="Enter the second number"
+int x= "Enter the first number:"
+int y="Enter the second number:"
 int z= x*y;
-print("The dot product is",z)
+print("The dot product of 2 numbers is",z)
+//a comment
